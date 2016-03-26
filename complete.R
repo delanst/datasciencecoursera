60a2fefe495d23a9da540b1c396e3b2f7b8ea988
delans1 <Y8HZ1y$CWZPnJ$4s>
2016-03-27 00:11:56 +0100

    Week 2 Programming in R

---
+++ complete.R
@@ -0,0 +1,9 @@
+complete <- function(directory, id = 1:332) {
+  
+  sumCompleteCases <- function(id) {
+    path <- file.path(directory, paste(sprintf("%03d", as.numeric(id)), ".csv", sep=""))
+    return (sum(complete.cases(read.csv(path))))
+  }
+  return (data.frame(id=id, nobs=sapply(id, sumCompleteCases)))
+  
+}
